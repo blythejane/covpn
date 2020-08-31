8bf100d5f1375a7533b94c8007306ba7db4b7fa7
Blythe Adamson <blythejane@gmail.com>
2020-08-30 18:16:08 -0700

    Create endpoint_production.R
    This file is sourced from covid_registry_analysis.Rmd

---
+++ endpoint_production.R
@@ -0,0 +1,12 @@
+
+ore.connect("", host="", 
+            password="", 
+            service_name="", all=TRUE)
+
+score_summary_query = "[insert query string here]"
+
+ore.sync(query = c("score_summary_query_result" = score_summary_query))
+
+oracle_production <- data.frame(score_summary_query_result)
+
+hist(oracle_production$SCORE_ENDPOINT, breaks = 50)
